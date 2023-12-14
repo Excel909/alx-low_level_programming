478d26c81f9ff5a491632d4014520aeb5dccea3b
Excellent Ayomide Badejo <excellentayomide@gmail.com>
2023-12-14 07:25:55 -0800

    done 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,12 @@
 
 /**
 * main - prints out a string via printf
-* Description - program that prints out a string and is followed by a new line
-* Return - 0 when no errors
+* Description: Program that prints out the string s followed by a new line
+* Return: int 0 when done and no errors
 */
 
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
